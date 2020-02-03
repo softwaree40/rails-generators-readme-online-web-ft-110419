71e7bf8c83b146580d44caab73e06c4e5546479d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-03 22:34:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200203221820) do
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "genre"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160113163910) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
