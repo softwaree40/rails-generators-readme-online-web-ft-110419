9e62ccfb83eb8a9acd8f5c697aa940486a629dd2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-03 22:34:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113163910) do
+ActiveRecord::Schema.define(version: 20160113163851) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160113163910) do
     t.string   "post_status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "category_id"
   end
 
 end
